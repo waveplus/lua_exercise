6691d4ffed9c8d19ca4614bf5089af4751ec715c
waveplus <alexismartie19890924@gmail.com>
2017-10-12 14:16:57 +0200

    excercise 3

---
+++ 3.lua
@@ -0,0 +1,12 @@
+mytable = setmetatable({key1 = "value1"}, {
+   __index = function(mytable, key)
+	
+      if key == "key2" then
+         return "metatablevalue"
+      else
+         return mytable[key]
+      end
+   end
+})
+
+print(mytable.key1,mytable.key2)
